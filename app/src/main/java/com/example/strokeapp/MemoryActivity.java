85ab76eb2ef491a48b75ff80c7f15389bfeb03b7
AngelaZhang123 <angelazhang3913@gmail.com>
2022-03-12 19:29:58 -0800

    Physical Rehab, memory, mental, emergency call UI updated

--- app/src/main/java/com/example/strokeapp/MemoryActivity.java
+++ app/src/main/java/com/example/strokeapp/MemoryActivity.java
@@ -2,13 +2,36 @@ package com.example.strokeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MemoryActivity extends AppCompatActivity {
 
+    public ImageButton homeButton, objectButton, locationButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory);
+
+        homeButton = (ImageButton) findViewById(R.id.homeButtonMem);
+        homeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemoryActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        objectButton = (ImageButton) findViewById(R.id.objectButton);
+        homeButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MemoryActivity.this, ObjectIdentifierActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/strokeapp/ObjectIdentifierActivity.java
@@ -0,0 +1,14 @@
+package com.example.strokeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObjectIdentifierActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_object_identifier);
+    }
+}
